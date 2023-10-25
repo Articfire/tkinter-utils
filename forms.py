9a446100ccbf9598070eb702a72416514c784f82
Martin F. Hurtado <martindrumer21@gmail.com>
2023-10-25 15:01:38 -0700

    Add __init__.py file and forms.py file. Added a Form parent class and FileSelectorForm as child class of this. FileSelectorForm is a form that selects a file and displays its name. Also you can close the form and extend it with code.

---
+++ forms.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog as fd
+from idlelib.tooltip import Hovertip
+
+class Form(object):
+    def __init__(self, title:str, size:str):
+        self.base = Tk()
+        self.base.geometry(size)    # example: '500x320'
+        self.base.title(title)
+
+class FileSelectorForm(Form):
+    def __init__(self, title:str, commands):
+        super().__init__(title, "380x105")
+        self.file_name = StringVar()
+
+        self.label_subtitle = Label(master=self.base, text="Selecciona el archivo a procesar", width=26, font=("Verdana", 15, "normal"))
+        self.label_subtitle.place(x=0, y=0)
+
+        self.entry_file_name = Entry(master=self.base, width=60)
+        self.entry_file_name.place(x=10, y=40)
+
+        self.button_file_name = Button(self.base, text='Seleccionar', width=15, bg='#0d6efd', fg='white')
+        self.button_file_name.config(command=self.GetFileName)
+        self.button_file_name.place(x=10, y=70)
+
+        self.button_process = Button(self.base, text='Procesar', width=15, fg='black')
+        # self.button_process.config(command=self.GetFileName)
+        self.button_process.place(x=130, y=70)
+
+        self.button_close = Button(self.base, text='Cerrar', width=15, fg='black')
+        self.button_close.config(command=self.base.destroy)
+        self.button_close.place(x=250, y=70)
+
+        self.base.mainloop()
+    
+    def GetFileName(self):
+        self.file_name = fd.askopenfilename()
+        self.entry_file_name.delete(0, len(self.entry_file_name.get()))
+        self.entry_file_name.insert(0, self.file_name.split('/')[::-1][0])
+
+form = FileSelectorForm("Prueba", None)
